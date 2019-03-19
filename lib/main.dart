c4109cd90407715f228ca85ba129ba990cb9b8a2
Pedro Pessoa <pessoa.pedro91@gmail.com>
2019-03-20 00:31:22 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello Rectangle"),
+          backgroundColor: Colors.green[900],
+        ),
+        body: HelloRectangle(),
+      ),
+    ),
+  );
+}
+
+class HelloRectangle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: Colors.greenAccent,
+        height: 400.0,
+        width: 500.0,
+        child: Center(child: Text("Hello")),
+      ),
+    );
+  }
+}
