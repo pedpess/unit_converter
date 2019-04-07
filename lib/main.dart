e641a8961565787b21fd36a1ee78b70b8dd71993
Pedro Pessoa <pessoa.pedro91@gmail.com>
2019-04-07 23:37:59 +0300

    Add remaning categories in a screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,22 @@
 import 'package:flutter/material.dart';
 
-import 'package:unit_converter/widgets/widgets.dart';
+import 'package:unit_converter/screens/screens.dart';
 
 const _categoryName = 'Cake';
 const _categoryIcon = Icons.cake;
 const _categoryColor = Colors.green;
 
 void main() {
-  runApp(UniConverterApp());
+  runApp(UnitConverterApp());
 }
 
-class UniConverterApp extends StatelessWidget {
+class UnitConverterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Unit Converter',
-      home: Scaffold(
-        backgroundColor: Colors.green[100],
-        body: Center(
-          child: Category(
-            name: _categoryName,
-            color: _categoryColor,
-            iconLocation: _categoryIcon,
-          ),
-        ),
-      ),
+      home: CategoryScreen(),
     );
   }
 }
