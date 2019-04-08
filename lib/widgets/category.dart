6dddabf29ad1b7010f1c03916acce5c3ec13cf14
Pedro Pessoa <pessoa.pedro91@gmail.com>
2019-04-09 00:38:48 +0300

    Add navigation

--- lib/widgets/category.dart
+++ lib/widgets/category.dart
@@ -3,23 +3,23 @@ import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 
 import 'package:unit_converter/models/models.dart';
+import 'package:unit_converter/screens/screens.dart';
 
 final _rowHeight = 100.0;
 final _borderRadius = BorderRadius.circular(_rowHeight / 2);
 
 class Category extends StatelessWidget {
-
   final IconData iconLocation;
   final String name;
   final ColorSwatch color;
   final List<Unit> units;
 
-  Category({
-    Key key,
-    @required this.iconLocation,
-    @required this.color,
-    @required this.units,
-    @required this.name})
+  Category(
+      {Key key,
+      @required this.iconLocation,
+      @required this.color,
+      @required this.units,
+      @required this.name})
       : assert(iconLocation != null),
         assert(units != null),
         assert(color != null),
@@ -27,7 +27,25 @@ class Category extends StatelessWidget {
         super(key: key);
 
   void _navigateToConverter(BuildContext context) {
-
+    Navigator.of(context)
+        .push(MaterialPageRoute<Null>(builder: (BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          elevation: 1.0,
+          title: Text(
+            name,
+            style: Theme.of(context).textTheme.display1,
+          ),
+          centerTitle: true,
+          backgroundColor: color,
+        ),
+        body: ConverterScreen(
+          color: color,
+          name: name,
+          units: units,
+        ),
+      );
+    }));
   }
 
   @override
@@ -40,29 +58,28 @@ class Category extends StatelessWidget {
           borderRadius: _borderRadius,
           highlightColor: color,
           splashColor: color,
-          onTap: () => print('I was tapped!'),
+          onTap: () => _navigateToConverter(context),
           child: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Icon(
-                    iconLocation,
-                    size: 60.0,
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Icon(
+                      iconLocation,
+                      size: 60.0,
+                    ),
                   ),
-                ),
-                Center(
-                  child: Text(
-                    name,
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.headline,
-                  ),
-                )
-              ],
-            )
-          ),
+                  Center(
+                    child: Text(
+                      name,
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.headline,
+                    ),
+                  )
+                ],
+              )),
         ),
       ),
     );
